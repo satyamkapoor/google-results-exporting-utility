1ae75007abc194d9d26a7ab47e2510b03d114ea5
Satyam Kapoor <me@satyamkapoor.com>
2015-10-04 01:13:47 +0530

    Added csv exporter

--- dd.php
+++ dd.php
@@ -2,34 +2,21 @@
 require_once('simple_html_dom.php');
 $quer=$_POST["query1"];
 $count=$_POST["count"];
+$option = $_POST["optradio"];
 $j=10;
 
-echo "Page 1";
-   
 
-$url  = 'http://www.google.com/search?hl=en&safe=active&tbo=d&site=&source=hp&q='.$quer;
-$html = file_get_html($url);
- $j=$j+10;
-$linkObjs = $html->find('h3.r a');
-foreach ($linkObjs as $linkObj) {
-    $title = trim($linkObj->plaintext);
-    $link  = trim($linkObj->href);
-    
-    // if it is not a direct link but url reference found inside it, then extract
-    if (!preg_match('/^https?/', $link) && preg_match('/q=(.+)&amp;sa=/U', $link, $matches) && preg_match('/^https?/', $matches[1])) {
-        $link = $matches[1];
-    } else if (!preg_match('/^https?/', $link)) { // skip if it is not a valid link
-        continue;    
-    }
-    
-    echo '<p>Title: ' . $title . '<br />';
-    echo 'Link: ' . $link . '</p>';    
-}
+   $m=0;
+
+$file = fopen("greu.csv","w");
 
 
 for($i=1;$i<$count;$i++)
 {
-	echo "Page ".$i."<br>";
+	if($option =="web"){
+  echo "Page ".$i."<br>";
+}
+
    
 
 $url  = 'http://www.google.com/search?hl=en&safe=active&tbo=d&site=&source=hp&q='.$quer.'&start='.$j;
@@ -47,9 +34,38 @@ foreach ($linkObjs as $linkObj) {
         continue;    
     }
     
+    $array1[$m]=$link;
+    $m++;
+
+    if($option == "web")
+    {
     echo '<p>Title: ' . $title . '<br />';
     echo 'Link: ' . $link . '</p>';    
+  }
+
+
 }
 
 }
-?>
+
+
+if($option == "csv")
+{
+
+foreach ($array1 as $line)
+  {
+  fputcsv($file,explode(',',$line));
+  }
+
+
+  ?>
+  <center>
+  <h3>CSV Generated</h3>
+
+<a href="file.php">Download</a>
+</center>
+  <?php
+
+}
+
+fclose($file); ?> 
